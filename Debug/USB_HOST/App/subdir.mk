7389d9da0223409b54a7871d8fde2a7cfc81e7b6
pankajverma190 <pankaj.verma190@gmail.com>
2021-06-17 18:24:25 +0530

    blink code f4

---
+++ Debug/USB_HOST/App/subdir.mk
@@ -0,0 +1,25 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../USB_HOST/App/usb_host.c 
+
+OBJS += \
+./USB_HOST/App/usb_host.o 
+
+C_DEPS += \
+./USB_HOST/App/usb_host.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+USB_HOST/App/%.o: ../USB_HOST/App/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: MCU GCC Compiler'
+	@echo $(PWD)
+	arm-none-eabi-gcc -mcpu=cortex-m4 -mthumb -mfloat-abi=hard -mfpu=fpv4-sp-d16 -DUSE_HAL_DRIVER -DSTM32F407xx -I"D:/CUBEMX/STM32F4/Bsic_blink_Bare_project/USB_HOST/App" -I"D:/CUBEMX/STM32F4/Bsic_blink_Bare_project/USB_HOST/Target" -I"D:/CUBEMX/STM32F4/Bsic_blink_Bare_project/Core/Inc" -I"D:/CUBEMX/STM32F4/Bsic_blink_Bare_project/Drivers/STM32F4xx_HAL_Driver/Inc" -I"D:/CUBEMX/STM32F4/Bsic_blink_Bare_project/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy" -I"D:/CUBEMX/STM32F4/Bsic_blink_Bare_project/Middlewares/ST/STM32_USB_Host_Library/Core/Inc" -I"D:/CUBEMX/STM32F4/Bsic_blink_Bare_project/Middlewares/ST/STM32_USB_Host_Library/Class/CDC/Inc" -I"D:/CUBEMX/STM32F4/Bsic_blink_Bare_project/Drivers/CMSIS/Device/ST/STM32F4xx/Include" -I"D:/CUBEMX/STM32F4/Bsic_blink_Bare_project/Drivers/CMSIS/Include"  -Og -g3 -Wall -fmessage-length=0 -ffunction-sections -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
---
+++ Debug/USB_HOST/Target/usbh_platform.d
@@ -0,0 +1,103 @@
+USB_HOST/Target/usbh_platform.o: ../USB_HOST/Target/usbh_platform.c \
+ ../USB_HOST/Target/usbh_platform.h \
+ D:/CUBEMX/STM32F4/Bsic_blink_Bare_project/USB_HOST/App/usb_host.h \
+ D:/CUBEMX/STM32F4/Bsic_blink_Bare_project/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
+ D:/CUBEMX/STM32F4/Bsic_blink_Bare_project/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f407xx.h \
+ D:/CUBEMX/STM32F4/Bsic_blink_Bare_project/Drivers/CMSIS/Include/core_cm4.h \
+ D:/CUBEMX/STM32F4/Bsic_blink_Bare_project/Drivers/CMSIS/Include/cmsis_version.h \
+ D:/CUBEMX/STM32F4/Bsic_blink_Bare_project/Drivers/CMSIS/Include/cmsis_compiler.h \
+ D:/CUBEMX/STM32F4/Bsic_blink_Bare_project/Drivers/CMSIS/Include/cmsis_gcc.h \
+ D:/CUBEMX/STM32F4/Bsic_blink_Bare_project/Drivers/CMSIS/Include/mpu_armv7.h \
+ D:/CUBEMX/STM32F4/Bsic_blink_Bare_project/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h \
+ D:/CUBEMX/STM32F4/Bsic_blink_Bare_project/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h \
+ D:/CUBEMX/STM32F4/Bsic_blink_Bare_project/Core/Inc/stm32f4xx_hal_conf.h \
+ D:/CUBEMX/STM32F4/Bsic_blink_Bare_project/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h \
+ D:/CUBEMX/STM32F4/Bsic_blink_Bare_project/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h \
+ D:/CUBEMX/STM32F4/Bsic_blink_Bare_project/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ D:/CUBEMX/STM32F4/Bsic_blink_Bare_project/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h \
+ D:/CUBEMX/STM32F4/Bsic_blink_Bare_project/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h \
+ D:/CUBEMX/STM32F4/Bsic_blink_Bare_project/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h \
+ D:/CUBEMX/STM32F4/Bsic_blink_Bare_project/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_exti.h \
+ D:/CUBEMX/STM32F4/Bsic_blink_Bare_project/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h \
+ D:/CUBEMX/STM32F4/Bsic_blink_Bare_project/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h \
+ D:/CUBEMX/STM32F4/Bsic_blink_Bare_project/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h \
+ D:/CUBEMX/STM32F4/Bsic_blink_Bare_project/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h \
+ D:/CUBEMX/STM32F4/Bsic_blink_Bare_project/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h \
+ D:/CUBEMX/STM32F4/Bsic_blink_Bare_project/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h \
+ D:/CUBEMX/STM32F4/Bsic_blink_Bare_project/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2c.h \
+ D:/CUBEMX/STM32F4/Bsic_blink_Bare_project/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2c_ex.h \
+ D:/CUBEMX/STM32F4/Bsic_blink_Bare_project/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2s.h \
+ D:/CUBEMX/STM32F4/Bsic_blink_Bare_project/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2s_ex.h \
+ D:/CUBEMX/STM32F4/Bsic_blink_Bare_project/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h \
+ D:/CUBEMX/STM32F4/Bsic_blink_Bare_project/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h \
+ D:/CUBEMX/STM32F4/Bsic_blink_Bare_project/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_spi.h \
+ D:/CUBEMX/STM32F4/Bsic_blink_Bare_project/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_hcd.h \
+ D:/CUBEMX/STM32F4/Bsic_blink_Bare_project/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_ll_usb.h
+
+../USB_HOST/Target/usbh_platform.h:
+
+D:/CUBEMX/STM32F4/Bsic_blink_Bare_project/USB_HOST/App/usb_host.h:
+
+D:/CUBEMX/STM32F4/Bsic_blink_Bare_project/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
+
+D:/CUBEMX/STM32F4/Bsic_blink_Bare_project/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f407xx.h:
+
+D:/CUBEMX/STM32F4/Bsic_blink_Bare_project/Drivers/CMSIS/Include/core_cm4.h:
+
+D:/CUBEMX/STM32F4/Bsic_blink_Bare_project/Drivers/CMSIS/Include/cmsis_version.h:
+
+D:/CUBEMX/STM32F4/Bsic_blink_Bare_project/Drivers/CMSIS/Include/cmsis_compiler.h:
+
+D:/CUBEMX/STM32F4/Bsic_blink_Bare_project/Drivers/CMSIS/Include/cmsis_gcc.h:
+
+D:/CUBEMX/STM32F4/Bsic_blink_Bare_project/Drivers/CMSIS/Include/mpu_armv7.h:
+
+D:/CUBEMX/STM32F4/Bsic_blink_Bare_project/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
+
+D:/CUBEMX/STM32F4/Bsic_blink_Bare_project/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h:
+
+D:/CUBEMX/STM32F4/Bsic_blink_Bare_project/Core/Inc/stm32f4xx_hal_conf.h:
+
+D:/CUBEMX/STM32F4/Bsic_blink_Bare_project/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h:
+
+D:/CUBEMX/STM32F4/Bsic_blink_Bare_project/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h:
+
+D:/CUBEMX/STM32F4/Bsic_blink_Bare_project/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+
+D:/CUBEMX/STM32F4/Bsic_blink_Bare_project/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h:
+
+D:/CUBEMX/STM32F4/Bsic_blink_Bare_project/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h:
+
+D:/CUBEMX/STM32F4/Bsic_blink_Bare_project/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h:
+
+D:/CUBEMX/STM32F4/Bsic_blink_Bare_project/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_exti.h:
+
+D:/CUBEMX/STM32F4/Bsic_blink_Bare_project/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h:
+
+D:/CUBEMX/STM32F4/Bsic_blink_Bare_project/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h:
+
+D:/CUBEMX/STM32F4/Bsic_blink_Bare_project/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h:
+
+D:/CUBEMX/STM32F4/Bsic_blink_Bare_project/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h:
+
+D:/CUBEMX/STM32F4/Bsic_blink_Bare_project/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h:
+
+D:/CUBEMX/STM32F4/Bsic_blink_Bare_project/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h:
+
+D:/CUBEMX/STM32F4/Bsic_blink_Bare_project/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2c.h:
+
+D:/CUBEMX/STM32F4/Bsic_blink_Bare_project/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2c_ex.h:
+
+D:/CUBEMX/STM32F4/Bsic_blink_Bare_project/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2s.h:
+
+D:/CUBEMX/STM32F4/Bsic_blink_Bare_project/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2s_ex.h:
+
+D:/CUBEMX/STM32F4/Bsic_blink_Bare_project/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h:
+
+D:/CUBEMX/STM32F4/Bsic_blink_Bare_project/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h:
+
+D:/CUBEMX/STM32F4/Bsic_blink_Bare_project/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_spi.h:
+
+D:/CUBEMX/STM32F4/Bsic_blink_Bare_project/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_hcd.h:
+
+D:/CUBEMX/STM32F4/Bsic_blink_Bare_project/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_ll_usb.h:
